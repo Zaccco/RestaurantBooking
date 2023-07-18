98a724375854a9564732b65a045bd684f6a30648
Zaccco <samuelcollskog98@gmail.com>
2023-07-18 16:14:03 +0000

    Add home view and base template

--- RestaurantBookingApp/views.py
+++ RestaurantBookingApp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import generic
+from .models import Reservation
+from django.contrib import messages
+
 
 # Create your views here.
+def home(request):
+    """
+    This is the view for the homepage that renders index.html
+    """
+    return render(request, 'index.html')
+
+
+def booking_page():
+    """
+    The view for the booking page. If the user is logged in the bookings page
+    will show up, if the user is not logged in, it will be redirected to the
+    login/signup page.
+    """
+    pass
