df89ccf4b52e0729286dbdeba5508fe44420fd3c
Zaccco <samuelcollskog98@gmail.com>
2023-07-18 12:16:37 +0000

    update allowed host to heroku

--- RestaurantBooking/settings.py
+++ RestaurantBooking/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['restaurant-booking-app-p4.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['restaurant-booking-app-p4-62d98244b1cc.herokuapp.com', 'localhost']
 
 
 # Application definition
