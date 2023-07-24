4e75301b35cb629d671b3d0066b37d607755ebf6
Zaccco <samuelcollskog98@gmail.com>
2023-07-24 16:52:21 +0000

    Debug set to false

--- RestaurantBooking/settings.py
+++ RestaurantBooking/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['restaurant-booking-app-p4-62d98244b1cc.herokuapp.com', 'localhost', '8000-zaccco-restaurantbookin-33mv0optcb3.ws-eu102.gitpod.io']
 
